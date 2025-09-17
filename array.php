30f6c26b4cc0aa5d3b99f41530f8c755ebca6c1a
dimasaditt <aditcraftyt@gmail.com>
2025-09-17 11:04:10 +0700

    commit

--- array.php
+++ array.php
@@ -60,29 +60,4 @@
     foreach ($daftarNilai[$mataKuliah] as $nilai) {
         echo $no++ . ". Nama: {$nilai[0]} | Nilai: {$nilai[1]} <br>";
     }
-
-    $arrNilai = [
-        ['Andi', 75],
-        ['Budi', 88],
-        ['Citra', 95],
-        ['Dinda', 70],
-        ['Farhan', 82],
-    ];
-
-    $totalNilai = 0;
-    foreach ($arrNilai as $data) {
-        $totalNilai += $data[1];
-    }
-
-    $rataRata = $totalNilai / count($arrNilai);
-
-    echo "Rata-rata nilai kelas: $rataRata <br>";
-    echo "Daftar siswa dengan nilai di atas rata-rata:<br>";
-
-    $no = 1;
-    foreach ($arrNilai as $data) {
-        if ($data[1] > $rataRata) {
-            echo $no++ . ". Nama: {$data[0]} | Nilai: {$data[1]} <br>";
-        }
-    }
 ?>
